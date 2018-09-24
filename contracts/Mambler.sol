1c9be975568d61f68db665fefe61aab1669a8f73
Jon Gaspar <jongaspar@Jons-MacBook-Pro.local>
2018-09-23 20:48:03 -0700

    testing fixing of email

--- contracts/Mambler.sol
+++ contracts/Mambler.sol
@@ -2,10 +2,11 @@ pragma solidity ^0.4.24;
 import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract Mambler is Ownable {
-    // Track the owner
-    address internal owner;
-
     function bet() {
+
+    }
+
+    function claimPrize() {
 
     }
 }
