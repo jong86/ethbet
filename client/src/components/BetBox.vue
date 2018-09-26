4aaf12163042809e3f9d5d025bbbd122c912d024
Jon Gaspar <jongaspar@Jons-MacBook-Pro.local>
2018-09-26 14:41:11 -0700

    got rid of vue starter component and added basics of new ones

---
+++ client/src/components/BetBox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="bet-box">
+    <input type="text"/>
+    <input type="number"/>
+    <button>Place Bet</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BetBox',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
