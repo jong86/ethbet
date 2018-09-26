2f7250cf8f428b8fb11e34642e96610cc0c6e23b
Jon Gaspar <jongaspar@Jons-MacBook-Pro.local>
2018-09-26 16:16:10 -0700

    place bet button console logs bet and address. added web3 and axios libraries

--- client/src/components/BetBox.vue
+++ client/src/components/BetBox.vue
@@ -1,34 +1,42 @@
 <template>
   <div class="bet-box">
-    <input type="text"/>
-    <input type="number"/>
-    <button>Place Bet</button>
+    <input
+      type="text"
+      v-model="betAddress"
+    />
+    <input
+      type="number"
+      v-model="betAmount"
+    />
+    <button
+      @click="placeBet"
+    >Place Bet</button>
   </div>
 </template>
 
 <script>
+import web3 from '../util/web3.js'
+
 export default {
   name: 'BetBox',
+  data: () => ({
+    betAddress: '',
+    betAmount: 0,
+  }),
   props: {
     msg: String
+  },
+  methods: {
+    placeBet() {
+      const { betAmount, betAddress } = this.$data
+      
+      
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
---
+++ client/src/util/web3.js
@@ -0,0 +1,2 @@
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"));
+export default web3;
