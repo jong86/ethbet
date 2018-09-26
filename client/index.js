a2300d3de9fdf04b818d556ca80dfaee03f78f11
Jon Gaspar <jongaspar@Jons-MacBook-Pro.local>
2018-09-26 12:15:03 -0700

    deleted old client stuff

--- client/index.js
+++
@@ -1,8 +0,0 @@
-[
-  'Components/Component.js',
-  'Components/BetBox.js',
-].forEach(url => {
-  var script = document.createElement("script");
-  script.src = url;
-  document.head.appendChild(script);  
-})
